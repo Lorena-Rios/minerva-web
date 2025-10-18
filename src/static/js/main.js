3b5ca9c155afad920f77aae90ac00a6cef1dca32
Lorena Rios <lorenadejesusrios@gmail.com>
2025-10-18 12:56:14 -0300

    feat: nova página de Cadastro

--- src/static/js/main.js
+++
@@ -1,3 +0,0 @@
-function openLogin() {
-    window.location.href = "./login"; 
-}
---
+++ src/static/js/scripts.js
@@ -0,0 +1,17 @@
+function openLogin() {
+    window.location.href = "./login/index.html"; 
+}
+
+
+tailwind.config = {
+            theme: {
+                extend: {
+                    colors: {
+                        'verde-escuro': '#81A86B',
+                        'verde-claro': '#B5CA8A',
+                        'roxo-escuro': '#9A5CAD',
+                        'roxo-claro': '#C8A9E0',
+                    }
+                }
+            }
+        }
