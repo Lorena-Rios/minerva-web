bf0bc9f52201679cf2b7a08cd9ac0c2fd63c10d6
Lorena Rios <lorenadejesusrios@gmail.com>
2025-10-24 23:05:59 -0300

    feat: Adição de tela de perfil

---
+++ public/js/functions/profile.js
@@ -0,0 +1,6 @@
+import { loadSidebar } from "/public/js/utils/loadPartial.js";
+import { toggleSidebar } from "/public/js/main.js";
+
+loadSidebar(); // Carrega o partial
+window.toggleSidebar = toggleSidebar; // Garante acesso global
+
